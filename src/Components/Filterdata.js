4d7c38479143afad100044789cb1eb0f5c2c3ff1
prathi-112 <sowmyagoud0304@gmail.com>
2024-03-24 15:49:40 +0530

    pagination changes

--- src/Components/Filterdata.js
+++ src/Components/Filterdata.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CountryCard from "./CountryCard";
 import { SearchContext } from "../Pages/Home";
 import { Pagination } from "@mui/material";
@@ -6,35 +6,40 @@ import { Pagination } from "@mui/material";
 function Filterdata() {
   const { userInput, regionChoice, apiData } = useContext(SearchContext);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 12; 
+  const [filteredData, setFilteredData] = useState([]);
+  const itemsPerPage = 12;
 
- 
+  useEffect(() => {
+    const filtered = apiData.filter((country) => {
+      const name = country.name.common.toLowerCase();
+      const input = userInput.toLowerCase();
+      return (
+        name.startsWith(input) &&
+        (regionChoice === "" || country.region === regionChoice)
+      );
+    });
+    setFilteredData(filtered);
+    setCurrentPage(1);
+  }, [userInput, regionChoice, apiData]);
+
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
 
- 
-  let filteredData = apiData.filter((country) => {
-    const name = country.name.common.toLowerCase();
-    const input = userInput.toLowerCase();
-    return name.startsWith(input) && (regionChoice === "" || country.region === regionChoice);
-  });
-
-
-  const displayedData = filteredData.slice(startIndex, endIndex).map((country, index) => (
-    <CountryCard
-      key={index}
-      flag={country.flags.png}
-      name={country.name.common}
-      population={country.population}
-      region={country.region}
-      capital={country.capital}
-    />
-  ));
-
+  const displayedData = filteredData
+    .slice(startIndex, endIndex)
+    .map((country, index) => (
+      <CountryCard
+        key={index}
+        flag={country.flags.png}
+        name={country.name.common}
+        population={country.population}
+        region={country.region}
+        capital={country.capital}
+      />
+    ));
 
   const totalPages = Math.ceil(filteredData.length / itemsPerPage);
 
-  
   const handlePageChange = (event, page) => {
     setCurrentPage(page);
   };
@@ -48,72 +53,53 @@ function Filterdata() {
             fontSize: "20px",
             textAlign: "center",
             letterSpacing: "0.7px",
-            
           }}
         >
-          <p>Not Country found matching your input or filter, please try something different.</p>
+          <p>
+            No countries found matching your input or filter. Please try
+            something different.
+          </p>
         </div>
       ) : (
         <>
           {displayedData}
-          <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
-            <br/><br/>
-           
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "20px",
+            }}
+          >
+            <div>
+              {!userInput && (
+                <Pagination
+                  count={totalPages}
+                  page={currentPage}
+                  onChange={handlePageChange}
+                  color="secondary"
+                  variant="outlined"
+                  shape="rounded"
+                  sx={{
+                    position: "fixed",
+                    bottom: 0,
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    zIndex: 9999,
+                    "& .MuiPaginationItem-root": {
+                      fontWeight: "bold",
+                      color: "black",
+                      backgroundColor: "white",
+                      width: "60px",
+                    },
+                    "& .Mui-selected": {
+                      backgroundColor: "white",
+                      color: "blue",
+                    },
+                  }}
+                />
+              )}
+            </div>
           </div>
-         <div>
-         
-         <div style={{marginLeft:"600px"}} >
-          {/* <Pagination
-              count={20}
-              page={currentPage}
-              onChange={handlePageChange}
-              color="secondary"
-              variant="outlined"
-               shape="rounded"
-               
-               sx={{
-                "& .MuiPaginationItem-root": {
-                  fontWeight: "bold",        
-                  color: "black", 
-                  position:"relative",          
-                  backgroundColor: "white",
-                  width:"70px",
-                  right:"270px",
-                
-                  
-                 
-                }
-              }}
-              
-            /> */}
-            <Pagination
-  count={20}
-  page={currentPage}
-  onChange={handlePageChange}
-  color="secondary"
-  variant="outlined"
-  shape="rounded"
-  sx={{
-    "& .MuiPaginationItem-root": {
-      fontWeight: "bold",        
-      color: "black", 
-      position:"relative",          
-      backgroundColor: "white",
-      width:"60px",
-      right:"270px",
-      '&.Mui-selected': {
-        backgroundColor:"blue",
-        color: "white" 
-      }
-    }
-  }}
-/>
-
-           
-
-
-          </div>
-         </div>
         </>
       )}
     </>
